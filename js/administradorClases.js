5436fad43c5730ddccd836c86853359d43b3d5ca
Alejandro Penalba <atapenalba@gmail.com>
2021-03-18 19:37:38 -0300

    maquetado pagina index y admin

---
+++ js/administradorClases.js
@@ -0,0 +1,14 @@
+export class  Juego{
+    constructor(id, nombre, descripcion, categoria, imagen, descuento, precio, publicado, destacado){
+        this.id =id;
+        this.nombre = nombre;
+        this.descripcion= descripcion;
+        this.categoria= categoria;
+        this.imagen =imagen;
+        this.descuento = descuento;
+        this.precio = precio;
+        this.publicado = publicado;
+        this.destacado = destacado
+    }
+    
+}
--- js/validaciones.js
+++ js/validaciones.js
@@ -106,3 +106,57 @@ formulario.addEventListener('submit', (e) => {
         document.getElementById('formulario-mensaje').classList.add('formulario-mensaje-activo');
     }
 });
+
+/** Validaciones form admin **/
+function validarId(elemento) {
+    console.log("en la funcion campo requerido");
+    if (elemento.value === "") {
+      elemento.className = "form-control is-invalid";
+      return false;
+    } else {
+      elemento.className = "form-control is-valid";
+      return true;
+    }
+  }
+
+function validarNombre(elemento) {
+    console.log("en la funcion campo requerido");
+    if (elemento.value === "") {
+      elemento.className = "form-control is-invalid";
+      return false;
+    } else {
+      elemento.className = "form-control is-valid";
+      return true;
+    }
+  }
+
+function validarDescripcion(elemento){
+    if(elemento.value != '' && elemento.value.length >= 10){
+        elemento.className = 'form-control is-valid';
+        return true;
+    }else{
+        elemento.className = "form-control is-invalid";
+        return false;
+    }
+  }
+
+function validarNumeros(elemento) {
+    if (elemento.value != "" && !isNaN(elemento.value)) {
+      elemento.className = "form-control is-valid";
+      return true;
+    } else {
+      elemento.className = "form-control is-invalid";
+      return false;
+    }
+  }
+
+  function validarSelect(elemento) {
+      console.log(elemento)
+    if (elemento.value != 0) {
+      elemento.className = "form-control is-valid";
+      return true;
+    } else {
+      elemento.className = "form-control is-invalid";
+      return false;
+    }
+  }
