62902ed30783a78fbca76acf46e96256376ced95
sil Medina <medina.silviapatricia@gmail.com>
2021-03-22 01:06:35 -0300

    se completa el registro de Usuario

--- js/app.js
+++ js/app.js
@@ -2,31 +2,33 @@ import {Juego} from './administradorClases.js'
 import { Usuario } from './usuario.js'
 
 (function(){
-   const usuarioLogueado = localStorage.getItem('usuarioLogueado');
-   if (usuarioLogueado != null){
-    document.getElementById('botonLogin').style.display = 'none';
-    document.getElementById('botonRegistro').style.display = 'none';
-    const usuario = JSON.parse(usuarioLogueado);
-    document.getElementById('infoUsuario').innerHTML = '<i class="far fa-user"></i> ' + usuario.email;
-    if(usuario.tipoUsuario == 'administrador'){
-        document.getElementById('botonAdministrador').style.display = 'block';
-    }
-    document.getElementById('botonCerrarSesion').style.display = 'block';  
-    document.getElementById('infoUsuario').style.display = 'block';
-   }else{
-    document.getElementById('botonCerrarSesion').style.display = 'none';  
-    document.getElementById('infoUsuario').style.display = 'none';
-    document.getElementById('botonAdministrador').style.display = 'none';
-    document.getElementById('botonLogin').style.display = 'block';
-    document.getElementById('botonRegistro').style.display = 'block';
-   }
-   document.getElementById('botonInicio').style.display = 'block';
-
-   cargarUsuariosRegistrados();
+    mostrarOcultarBotones();
+    cargarUsuariosRegistrados();
 })();
 
+function mostrarOcultarBotones(){
+    const usuarioLogueado = localStorage.getItem('usuarioLogueado');
+    if (usuarioLogueado != null){
+     document.getElementById('botonLogin').style.display = 'none';
+     document.getElementById('botonRegistro').style.display = 'none';
+     const usuario = JSON.parse(usuarioLogueado);
+     document.getElementById('infoUsuario').innerHTML = '<i class="far fa-user"></i> ' + usuario.email;
+     if(usuario.tipoUsuario == 'administrador'){
+         document.getElementById('botonAdministrador').style.display = 'block';
+     }
+     document.getElementById('botonCerrarSesion').style.display = 'block';  
+     document.getElementById('infoUsuario').style.display = 'block';
+    }else{
+     document.getElementById('botonCerrarSesion').style.display = 'none';  
+     document.getElementById('infoUsuario').style.display = 'none';
+     document.getElementById('botonAdministrador').style.display = 'none';
+     document.getElementById('botonLogin').style.display = 'block';
+     document.getElementById('botonRegistro').style.display = 'block';
+    }
+    document.getElementById('botonInicio').style.display = 'block';
+}
+
 function cargarUsuariosRegistrados(){
-    
     if(localStorage.getItem('usuariosRegistrados') == null){
         const objetoAdmin = new Usuario('admin', 'admin', 'admin','admin@epicgames.com','');
         let arrayUsuarios = [objetoAdmin];
--- js/registro.js
+++ js/registro.js
@@ -2,6 +2,10 @@
 import { Usuario } from './usuario.js'
 
 (function(){
+    mostrarOcultarBotones();
+ })();
+
+ function mostrarOcultarBotones(){
     const usuarioLogueado = localStorage.getItem('usuarioLogueado');
     if (usuarioLogueado != null){
      document.getElementById('botonLogin').style.display = 'none';
@@ -21,12 +25,40 @@ import { Usuario } from './usuario.js'
      document.getElementById('botonRegistro').style.display = 'block';
     }
     document.getElementById('botonInicio').style.display = 'block';
- })();
-
- function registrarUsuario(event){
-     event.preventDefault();
-     alert('registro')
  }
+ window.registrarUsuario = function(event){
+    event.preventDefault();
+
+    // validar formulario
+
+    let usuario = document.getElementById('usuario').value;
+    let nombre = document.getElementById('nombre').value;
+    let password = document.getElementById('password').value;
+    let correo = document.getElementById('correo').value;
+    let telefono = document.getElementById('telefono').value;
+
+    let nuevoUsuario = new Usuario(usuario,nombre,password,correo,telefono);
+
+    if(localStorage.getItem('usuariosRegistrados') != null){
+        let usuariosRegistrados = JSON.parse(localStorage.getItem('usuariosRegistrados'));
+        let usuarioPorEmail = buscarUsuarioPorEmail(usuariosRegistrados,correo);
+        if(typeof usuarioPorEmail === 'undefined'){
+            usuariosRegistrados.push(nuevoUsuario);
+            localStorage.removeItem('usuariosRegistrados');
+            localStorage.setItem('usuariosRegistrados', JSON.stringify(usuariosRegistrados));
+        }else{
+            alert('El email ingresado ya existe');
+        }
+      
+    }
+}
+
+function  buscarUsuarioPorEmail(usuariosRegistrados, email){
+    let usuario = usuariosRegistrados.find(usuario=> {
+        return usuario.email === email
+      });
+    return usuario;
+}
 
 
 // formulario
